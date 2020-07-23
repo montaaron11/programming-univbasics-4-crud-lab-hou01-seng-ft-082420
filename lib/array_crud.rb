277697827f3aaae2123078b95f2f45cdf576891e
Aaron Montgomery <montaaron11@gmail.com>
2020-07-23 05:19:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   [1, 2, 3, 4]
-  add_element_to_end_of_array.push (4)
+  add_element_to_end_of_array << (4)
 end
 
 def add_element_to_start_of_array(array, element)
